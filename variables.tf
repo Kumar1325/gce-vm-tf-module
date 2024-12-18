aefaaced4f8838b22806eacbb915850cf32e11e4
Kumar1325 <Kumar1325@users.noreply.github.com>
2024-12-17 22:27:21 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,73 @@
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+
+variable "credentials_file" {
+  description = "Path to the GCP service account JSON credentials"
+  type        = string
+}
+
+variable "region" {
+  description = "Region where the instance will be deployed"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone where the instance will be deployed"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name of the VM instance"
+  type        = string
+}
+
+# Machine Type
+variable "machine_type" {
+  description = "Machine type of the VM instance"
+  type        = string
+
+  validation {
+    condition = (
+      !var.enable_confidential_vm || (
+        contains(["n2d", "n2", "e2"], split("-", var.machine_type)[0])
+      )
+    )
+    error_message = <<EOT
+Confidential VMs are only supported on N2D, N2, and E2 machine families.
+Please select a machine type like n2-standard-2, e2-medium, or n2d-highmem-4.
+EOT
+  }
+}
+
+variable "image" {
+  description = "Image to use for the VM boot disk"
+  type        = string
+}
+
+variable "network" {
+  description = "The network to attach to the VM instance"
+  type        = string
+}
+
+variable "subnetwork" {
+  description = "The subnetwork to attach to the VM instance"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to associate with the instance"
+  type        = list(string)
+  default     = []
+}
+
+variable "metadata" {
+  description = "Metadata to attach to the instance"
+  type        = map(string)
+  default     = {}
+}
+
 variable "enable_iap" {
   description = "Enable IAP tunneling for the VM"
   type        = bool
